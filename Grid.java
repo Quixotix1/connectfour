b477b99bb001cd55b647fb278f07e64c5d206a9d
SolidBrick <138042888+SolidBrick@users.noreply.github.com>
2024-01-18 10:41:10 -0500

    Update Grid.java
    changed connect_4 to connect4

--- Grid.java
+++ Grid.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.connect_4;
+package com.mycompany.connect4;
 import java.util.ArrayList;
 import javafx.scene.paint.Color;
 import javafx.scene.input.KeyCode;
