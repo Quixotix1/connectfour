fedb375b8ee5ee96dcc2796a72fd4b71cdf6cc61
Quixotix1 <49210797+Quixotix1@users.noreply.github.com>
2024-01-19 09:45:22 -0500

    Removed X and Y getter methods
    Circle already has getter methods for its location so I removed the methods for clarity.

--- Piece.java
+++ Piece.java
@@ -8,28 +8,20 @@ import javafx.scene.shape.Circle;
  * @author josh2211
  */
 public class Piece extends Circle {
-    private final double xLoc;
-    private final double yLoc;
     private final boolean isRed;
     
     public Piece(double xLoc, double yLoc, boolean isRed) {
         super(50, isRed ? Color.RED : Color.YELLOW);
-        this.xLoc = xLoc;
-        this.yLoc = yLoc;
         this.isRed = isRed;
         setCenterX(xLoc);
         setCenterY(yLoc);
     }
     
-    public double getX() {
-        return xLoc;
-    }
-    
-    public double getY() {
-        return yLoc;
-    }
-    
     public boolean getIsRed() {
         return isRed;
     }
+    
+    /**
+     * getCenterX() and getCenterY() are built in getter methods for x and y coordinates
+     */
 }
