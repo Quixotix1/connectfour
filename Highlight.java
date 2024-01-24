3b921b213e8799f224bedfbf0795e17ae27cc55c
kate8210 <146100099+kate8210@users.noreply.github.com>
2024-01-24 10:21:28 -0500

    Update Highlight.java

--- Highlight.java
+++ Highlight.java
@@ -45,30 +45,36 @@ public class Highlight{
     }
     
     //call getmousepos first
-    public Circle ifDrawGhostPiece(Group group){
+    public Circle ifDrawGhostPiece(Group group, int[] piecesInColumn){
         //check which column the mouse is in
         //draw a ghost piece in the correct location
         if(isThereAPiece){
             group.getChildren().remove(ghostPiece);
             isThereAPiece = false;
         }
-        int column = 0;
-        int row = 0;
+        
+        int column = 0;//indicates x coor to draw circle
+        int row = 0;//indicates y coor to draw circle
+        System.out.println(mouseLocation[0]);
+        System.out.println(mouseLocation[1]);
+        
         for (int i = 1; i < 8; i++){//check which column
             if (width/7*i > mouseLocation[0] && mouseLocation[0] > width/7*i-1){
                 column = i;
+                row = piecesInColumn[i - 1] + 1;
                 break;
             }
         }
-        for (int i = 1; i < 7; i++){//check which row
-            if (height/6*i > mouseLocation[1] && mouseLocation[1] > height/6*i-1){
-                row = i;
-                break;
-            }
-        }
+        //column = 2 + 3 pieces 
+        
         //draw piece at those coordinates
-        ghostPiece.setCenterX(column);
-        ghostPiece.setCenterY(row);
+        //ghostPiece.setCenterX(column);
+        //ghostPiece.setCenterY(row);
+        //ghostPiece.setRadius(radius);
+        System.out.println(column);
+        
+        ghostPiece.setCenterX(mouseLocation[0]);
+        ghostPiece.setCenterY(mouseLocation[1]);
         ghostPiece.setRadius(radius);
         
         if(colour){//pick colour
@@ -78,7 +84,7 @@ public class Highlight{
             ghostPiece.setFill(Color.YELLOW);
         }
         setColour();
-        ghostPiece.setOpacity(0.5);
+        ghostPiece.setOpacity(0.25);
         
         isThereAPiece = true;
         
@@ -109,12 +115,15 @@ public class Highlight{
     public double[] getMousePos(){//done...?
         Pane root = new Pane();
         root.setOnMouseMoved(new EventHandler<MouseEvent>(){
-            public void handle(MouseEvent event){
-                mouseLocation[0] = event.getX();
-                mouseLocation[1] = event.getY();
+            @Override public void handle(MouseEvent event){
+                //mouseLocation[0] = event.getX();
+                //mouseLocation[1] = event.getY();
+                
+                
             }
         });
-        
+        mouseLocation[0] = 400;
+        mouseLocation[1] = 100;
         return mouseLocation;
     }
     
