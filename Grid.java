34288ad08972e8e34f8ea963c468400f098b4e3f
pres1680 <146100034+pres1680@users.noreply.github.com>
2024-01-29 16:39:35 -0500

    Update Grid.java
    Restart works properly. Game is now fully functional

--- Grid.java
+++ Grid.java
@@ -1,238 +1,222 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.connect4;
+
 import java.util.ArrayList;
-import javafx.util.Pair;
-import javafx.scene.paint.Color;
-import javafx.scene.input.KeyCode;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.text.Text;
-import java.util.Map;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Line;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
-        
+import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+
+
+
 /**
- *
- * @author terr3890
+ * JavaFX App
  */
-public class Grid {
-    private int width;
-    private int height;
-    private double spaceWidth;
-    private double spaceHeight;
-    private double xOffset;
-    private double yOffset;
-    private double screenH;
-    private Piece[][] grid;
-    private ArrayList<Pair<Integer, Integer>> connectedCoords = new ArrayList<>();
-    private ArrayList<Piece> pieceList = new ArrayList<>();
-    private int[] piecesInColumn = new int[7];
-    
-    public Grid (int w, int h, double spaceW, double spaceH, double Xoffset, double Yoffset, double screenH) {
-        width = w;
-        height = h;
-        spaceWidth = spaceW;
-        spaceHeight = spaceH;
-        this.xOffset = Xoffset;
-        this.yOffset = Yoffset;
-        this.screenH = screenH;
-        grid = new Piece[width][height];
-    }
-    
-    public int getHeight() {
-        return height;
-    }
-    
-    public int getWidth() {
-        return width;
-    }
-    
-    public Piece getPiece(int x, int y) {
-        return grid[x][y];
-    }
-        
-    public int[] getPiecesInColumn(){
-        return piecesInColumn;
-    }
-        
-    public boolean isTaken(int x, int y) {
-        return !(grid[x][y] == null);
-    }
-    
-    public ArrayList<Piece> getPieceList() {
-       return pieceList;
-    }
-    
-    public Group placePiece(int x, boolean isRed, Group g) {
-    
+public class App extends Application {
+    //below are some constants
+    static final int COLUMN_NUMBER = 7;
+    static final int ROW_NUMBER = 6;
+    Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+    final double screenWidth = screenBounds.getWidth();
+    final double screenHeight = screenBounds.getHeight();
+    final double xOffset = (screenWidth / (COLUMN_NUMBER + 4)) * 2;
+    final double yOffset = (screenHeight / (ROW_NUMBER + 2));
+    final double spaceWidth = (screenWidth / (COLUMN_NUMBER + 4));
+    final double spaceHeight = (screenHeight / (ROW_NUMBER + 2));
+    final double boardWidth = screenWidth - xOffset * 2;
+    final double boardHeight = screenHeight - yOffset * 2;
+    boolean redTurn = true;
+    boolean gameOver = false;
+    Text gameOverText;
 
-        if (piecesInColumn[x] == 6) {
-            Error OverlapError = new Error("Overlapping piece");
-            throw OverlapError;
-        } else {
-            int y = piecesInColumn[x];
-            //        System.out.println("debug " + x + " " + y);
-            Piece p = new Piece(x * spaceWidth + xOffset + spaceWidth / 2, screenH - (y * spaceHeight) - yOffset - spaceHeight / 2, isRed); 
-            grid[x][y] = p;
-            pieceList.add(p);
-            g.getChildren().add(grid[x][y]);
-            piecesInColumn[x] += 1; //so we know how many pieces are already in each column
-            
-            return g;
-        } 
-    }
+    Group root = new Group(); //these things must be global otherwise compiler gets mad
+    Scene gameScene = null;
     
-    public boolean checkConnect4(int x) {
-        int y = piecesInColumn[x] - 1;
-        Piece p = grid[x][y];
-        boolean pieceIsRed = p.getIsRed(); //p.getIsRed() after or whatever method
-        return checkRow(x, y, p, pieceIsRed) || checkColumn(x, y, p, pieceIsRed) || checkDiagUpRight(x, y, p, pieceIsRed) || checkDiagUpLeft(x, y, p, pieceIsRed);
-    }
+    EventHandler<MouseEvent> mouseClickHandler;
     
-    private boolean checkRow(int x, int y, Piece p, boolean pieceIsRed) {
-        int connected = 1;
-        int x1 = x; //checks pieces to left
-        int x2 = x; // "" to right
-        boolean checkLeft = true; //bool to continue checking left
-        boolean checkRight = true; //"" right
-        while (true) {
-            x1 -= 1;
-            x2 += 1;
-            if (connected == 4) return true;
-            if (x1 >= 0 && checkLeft) {
-                if (grid[x1][y] != null) {
-                    if (grid[x1][y].getIsRed() == pieceIsRed) {
-                        connected += 1;
-                        connectedCoords.add(new Pair<>(x1, y));
-                    }
-                    else checkLeft = false;
-                } else checkLeft = false;
-            } else checkLeft = false;
-            if (x2 < width && checkRight) {
-                if (!(grid[x2][y] == null)) {
-                    if (grid[x2][y].getIsRed() == pieceIsRed) {
-                        connected += 1;
-                        connectedCoords.add(new Pair<>(x2, y));
-                    }
-                    else checkRight = false;
-                } else checkRight = false;
-            } else checkRight = false;
-            if (!(checkRight || checkLeft)) break;
+    @Override
+    public void start(Stage primaryStage) {
+        Scene startScene;
+        
+        Group startGroup = new Group();
+        ArrayList<Line> verticalLines = new ArrayList<>();
+        ArrayList<Line> horizontalLines = new ArrayList<>();
+        
+        //Initialize other classes
+        Grid grid = new Grid(COLUMN_NUMBER, ROW_NUMBER, spaceWidth, spaceHeight, xOffset, yOffset, screenHeight);
+        Highlight highlight = new Highlight(true, 50, boardWidth, boardHeight , grid);
+        
+        //Starting group setup
+        Label titleLabel = new Label("Connect 4");
+        titleLabel.setFont(new Font("Arial", 350));
+        
+        //titleLabel.setBackground(new Background(new BackgroundFill(Color.PINK, CornerRadii.EMPTY, Insets.EMPTY)));
+        Button startButton = new Button("Start");
+        startButton.setDefaultButton(true); //fun fact, this triggers the button if you hit enter
+        startButton.setLayoutX((screenWidth/2) - 150);
+        startButton.setLayoutY((screenHeight / 3 * 2) - 100);
+        startButton.setPrefSize(300, 100);
+        startButton.setFont(new Font("Arial", 60));
+        
+        startGroup.getChildren().add(titleLabel);
+        startGroup.getChildren().add(startButton);
+        
+        for(int i = 0; i < COLUMN_NUMBER + 1; i++) //initialize lines for board
+        {
+            verticalLines.add(new Line());
+            if (i < COLUMN_NUMBER) 
+            {
+                horizontalLines.add(new Line());
+            }   
         }
-        clearConnectedList();
-        return false;
-    }
-    
-    private boolean checkColumn(int x, int y, Piece p, boolean pieceIsRed) {
-        int connected = 1;
+        
+        initVerticalLines(verticalLines);
+        initHorizontalLines(horizontalLines);
 
-        while (true) {
-            y -= 1;
-            if (connected == 4) return true;
-            else if (y >= 0) {
-                if (grid[x][y].getIsRed() == pieceIsRed) {
-                    connected += 1;
-                    connectedCoords.add(new Pair<>(x, y));
+        //Root group setup
+        for (int i = 0; i < verticalLines.size(); i++)
+        {
+           root.getChildren().add(verticalLines.get(i)); 
+        }
+        for (int i = 0; i < horizontalLines.size(); i++)
+        {
+            root.getChildren().add(horizontalLines.get(i));
+        }
+        Button restartButton = new Button("Restart");
+        restartButton.setLayoutX(screenWidth/8*7);
+        restartButton.setLayoutY(screenHeight/5);
+        restartButton.setPrefSize(100, 50);
+        restartButton.setFont(new Font("Arial", 20));
+        
+         mouseClickHandler = new EventHandler<>() { 
+         @Override 
+         public void handle(MouseEvent e) {
+            // if (!gameOver)
+             //{
+                int index = findIndex(e.getX());
+                try
+                {
+                    root = grid.placePiece(index, redTurn, root);
+                    if(!grid.checkConnect4(index)) redTurn = highlight.setColour();
+                    else
+                    {
+                        gameScene.removeEventHandler(MouseEvent.MOUSE_CLICKED, mouseClickHandler);
+                        gameOver = true;
+                        root.getChildren().add(restartButton);
+                        restartButton.setDefaultButton(true);
+                        if (redTurn){gameOverText = new Text("Game Over, Red Wins!");} else {gameOverText = new Text("Game Over, Yellow Wins!");}
+                        gameOverText.setX((screenWidth / 2) - 350);
+                        gameOverText.setY(screenHeight / 9);
+                        gameOverText.setStyle("-fx-font: 50 arial;");
+                        root.getChildren().add(gameOverText);
+                    }
+                } catch(Error OverlapError)
+                {
+                    System.out.println("That column is full.");
                 }
-                else break;
-            } else break;
-        }
-        clearConnectedList();
-        return false;
+                catch(Exception ArraryIndexOutOfBoundsException)
+                {
+                    System.out.println("That move is out of bounds!"); //this doesn't catch perfectly accurately. This is a truncation issue; to be fixed
+                }
+             }
+            //}
+          };  
+          
+
+         
+
+       
+        
+        startScene = new Scene(startGroup, screenWidth, screenHeight);
+        startScene.setFill(Color.PINK);
+        gameScene = new Scene(root, screenWidth, screenHeight);
+        gameScene.setFill(Color.LIGHTGREY);
+        gameScene.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseClickHandler);
+        primaryStage.setScene(startScene);
+        primaryStage.setWidth(screenWidth);
+        primaryStage.setHeight(screenHeight);
+        primaryStage.setX(screenBounds.getMinX());
+        primaryStage.setY(screenBounds.getMinY());
+        primaryStage.setTitle("Primary Window");
+        primaryStage.show();
+
+        //printGhostPieces
+        highlight.getMousePos(gameScene, root, grid.getPiecesInColumn(), xOffset, yOffset); //should we change this method name? Kind of misleading
+        
+        startButton.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){
+              startButton.setDefaultButton(false);
+              primaryStage.setScene(gameScene);
+            }
+          });
+        
+        restartButton.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){
+                restart(grid, restartButton, grid.getPieceList(), highlight);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
     
-    private boolean checkDiagUpRight(int x, int y, Piece p, boolean pieceIsRed) {
-        int connected = 1;
-        int x1 = x; //checks pieces to left
-        int x2 = x; // "" to right
-        int y1 = y;
-        int y2 = y;
-        boolean checkLeft = true; //bool to continue checking left
-        boolean checkRight = true; //"" right
-        while (true) {
-            x1 -= 1;
-            x2 += 1;
-            y1 -= 1;
-            y2 += 1;
-            
-            if (connected == 4) return true;
-            if (x1 >= 0 && checkLeft && y1 >= 0) {
-                if (grid[x1][y1] != null) {
-                    if (grid[x1][y1].getIsRed() == pieceIsRed) {
-                        connected += 1;
-                        connectedCoords.add(new Pair<>(x1, y1));
-                    }
-                    else checkLeft = false;
-                } else checkLeft = false;
-            } else checkLeft = false;
-            if (x2 < width && checkRight && y2 < height) {
-                if (grid[x2][y2] != null) {
-                    if (grid[x2][y2].getIsRed() == pieceIsRed) {
-                        connected += 1;
-                        connectedCoords.add(new Pair<>(x2, y2));
-                    }
-                    else checkRight = false;
-                } else checkRight = false;
-            } else checkRight = false;
-            if (!(checkRight || checkLeft)) break;
-        }
-        clearConnectedList();
-        return false;
-    }
-    
-    private boolean checkDiagUpLeft(int x, int y, Piece p, boolean pieceIsRed) {
-        int connected = 1;
-        int x1 = x; //checks pieces to left
-        int x2 = x; // "" to right
-        int y1 = y;
-        int y2 = y;
-        boolean checkLeft = true; //bool to continue checking left
-        boolean checkRight = true; //"" right
-        while (true) {
-            x1 -= 1;
-            x2 += 1;
-            y1 += 1;
-            y2 -= 1;
-            
-            if (connected == 4) return true;
-            if (x1 >= 0 && checkLeft && y1 < height) {
-                if (grid[x1][y1] != null) {
-                    if (grid[x1][y1].getIsRed() == pieceIsRed) {
-                        connected += 1;
-                        connectedCoords.add(new Pair<>(x1, y1));
-                    } else checkLeft = false;
-                } else checkLeft = false;
-            } else checkLeft = false;
-            if (x2 < width && checkRight && y2 >= 0) {
-                if (grid[x2][y2] != null) {
-                    if (grid[x2][y2].getIsRed() == pieceIsRed) {
-                        connected += 1;
-                        connectedCoords.add(new Pair<>(x2, y2));
-                    } else checkRight = false;
-                } else checkRight = false;
-            } else checkRight = false;
-            if (!(checkRight || checkLeft)) break;
-        }
-        clearConnectedList();
-        return false;
-    }
-    
-    private void clearConnectedList() {
-        connectedCoords = new ArrayList<>();
-    }
-    
-    public ArrayList<Pair<Integer, Integer>> getConnectedCoords() {
-        return connectedCoords;
-    }
-    
-    public void restart()
+    private void initVerticalLines(ArrayList<Line> verticalLines)
     {
-       piecesInColumn = new int[width];
-       grid = new Piece[width][height]; //this does remove the old information from the memory right? Don't want to be wasteful...
-       clearConnectedList();
+        for (int i = 0; i < verticalLines.size(); i++) //vertical lines
+        {
+            verticalLines.get(i).setStartX((screenWidth / (COLUMN_NUMBER + 4)) * (i + 2)); //change this to col and row num
+            verticalLines.get(i).setEndX((screenWidth / (COLUMN_NUMBER + 4)) * (i + 2)); // maybe use a common variable for these two
+            verticalLines.get(i).setStartY(screenHeight / (ROW_NUMBER + 2));
+            verticalLines.get(i).setEndY(screenHeight / (ROW_NUMBER + 2) * (ROW_NUMBER + 1));
+            verticalLines.get(i).setStrokeWidth(3.0);
+        }
     }
+    
+    private void initHorizontalLines(ArrayList<Line> horizontalLines)
+    {
+        for (int i = 0; i < horizontalLines.size(); i++)
+        {
+            horizontalLines.get(i).setStartX((screenWidth / (COLUMN_NUMBER + 4)) * 2); 
+            horizontalLines.get(i).setEndX(screenWidth / (COLUMN_NUMBER + 4) * (9));
+            horizontalLines.get(i).setStartY((screenHeight / (ROW_NUMBER + 2)) * (i + 1));
+            horizontalLines.get(i).setEndY((screenHeight / (ROW_NUMBER + 2)) * (i + 1));
+            horizontalLines.get(i).setStrokeWidth(3.0);
+        }
+    }
+    
+    private int findIndex(double mouseX)
+    {
+         return (int) ((mouseX - (screenWidth / (COLUMN_NUMBER + 4) * 2)) / (screenWidth / (COLUMN_NUMBER + 4)));
+    }
+    
+    private void restart(Grid grid, Button restartButton, ArrayList<Piece> pieceList, Highlight highlight)
+    {
+        grid.restart();
+        gameScene.removeEventHandler(MouseEvent.MOUSE_CLICKED, mouseClickHandler);
+        root.getChildren().remove(restartButton);
+        for (int i = 0; i < pieceList.size(); i++)
+        {
+            root.getChildren().remove(pieceList.get(i));
+        }
+        highlight.getMousePos(gameScene, root, grid.getPiecesInColumn(), xOffset, yOffset);
+        gameOver = false;
+        gameScene.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseClickHandler);
+        root.getChildren().remove(gameOverText);
+    }
+
 }
